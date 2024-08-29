527b5c12f3b9bfd58e58fae15f83e14697cbe472
Andre Moreira <andre.moreira06205@gmail.com>
2024-08-29 19:48:16 -0300

    acabei

--- src/App.js
+++ src/App.js
@@ -31,12 +31,13 @@ function BasicExample() {
   <Card>
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/79/16/19961587.jpg" />
       <Card.Body>
-        <Card.Title>Coraline</Card.Title>
+        <Card.Title>Coraline
+        </Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Henry Selick,
+        2009 
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
@@ -45,12 +46,11 @@ function BasicExample() {
     <Card>
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/93/20/20120876.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>The Godfather</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Coppola, 1972
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
@@ -59,54 +59,50 @@ function BasicExample() {
   <Card>
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/84/28/19962110.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>O Rei Leão</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Rob Minkoff, 1994
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/16/48/20083748.jpg" />
+      <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/pictures/21/05/07/10/59/3500748.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Luca</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Enrico Casarosa, 2021
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/pictures/19/04/10/19/44/2904073.jpg" />
+      <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/pictures/22/03/02/19/26/3666027.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Batman</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Matt Reeves, 2022
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/30/21/19874092.jpg" />
+      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/pictures/14/06/26/18/10/332319.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Superman</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Richard Donner, 1978
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
@@ -117,14 +113,13 @@ function BasicExample() {
 <Row>
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/87/87/75/19962458.jpg" />
+      <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/87/29/07/19873973.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Gladiador</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Ridley Scott, 2000
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
@@ -133,12 +128,11 @@ function BasicExample() {
     <Card>
       <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/54/04/20150812.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Shrek</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Vicky Jenson, 2001
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
@@ -147,54 +141,50 @@ function BasicExample() {
   <Card>
       <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/87/87/63/19962446.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>A Vida É Bela</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Roberto Benigni, 1997
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/86/98/32/19870786.jpg" />
+      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/pictures/15/05/14/14/20/365361.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Divertida Mente</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Pete Docter, 2016
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/66/66/20156966.jpg" />
+      <Card.Img variant="top" src="https://br.web.img2.acsta.net/c_310_420/pictures/16/01/28/19/55/412201.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Deadpool</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Tim Miller, 2016
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
 
   <Col>
   <Card>
-      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/92/91/47/20224867.jpg" />
+      <Card.Img variant="top" src="https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/89/56/94/20055685.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Titanic</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          James Cameron, 1998
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Avaliar</Button>
       </Card.Body>
     </Card>
   </Col>
